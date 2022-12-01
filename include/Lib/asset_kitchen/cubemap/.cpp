76583a98d48fc98b327f30bc98d49d9fa51613d3
bekorn <cbaenrlki@gmail.com>
2022-12-01 23:58:40 +0300

    transposes environment maps to reduce load cost

--- include/Lib/asset_kitchen/cubemap/.cpp
+++ include/Lib/asset_kitchen/cubemap/.cpp
@@ -41,32 +41,49 @@ namespace Cubemap
 	LoadedData Load(Description const & description)
 	{
 		auto image_file = File::LoadImage(description.path);
-		auto face_dimensions = image_file.dimensions / i32x2{6, 1};
 
-		auto buffer = ByteBuffer(image_file.buffer.size);
-		auto dst_data = buffer.data_as<u8>();
-		auto dst_idx = 0;
-		auto src_data = image_file.buffer.data_as<u8>();
-		for (auto face = 0; face < 6; ++face)
-		{
-			auto src_idx = face * face_dimensions.x * image_file.channels;
-			for (auto y = 0; y < face_dimensions.y; ++y)
-			{
-				std::memcpy(dst_data + dst_idx, src_data + src_idx, face_dimensions.x * image_file.channels);
-				dst_idx += face_dimensions.x * image_file.channels;
-				src_idx += image_file.dimensions.x * image_file.channels;
-			}
-		}
-
-		return LoadedData{
-			.data = move(buffer),
-			.face_dimensions = face_dimensions,
+		auto loaded_data = LoadedData{
 			.channels = image_file.channels,
 			.is_sRGB = false,
 			.levels = description.levels,
 			.min_filter = description.min_filter,
 			.mag_filter = description.mag_filter,
 		};
+
+		if (6 * image_file.dimensions.x == image_file.dimensions.y)
+		{
+			// faces are stacked vertically (face pixels are seperate)
+			loaded_data.face_dimensions = image_file.dimensions / i32x2{1, 6};
+			loaded_data.data = move(image_file.buffer);
+		}
+		else if (image_file.dimensions.x == 6 * image_file.dimensions.y)
+		{
+			// faces are stacked horizontally (face pixels are interleved)
+			auto face_dimensions = image_file.dimensions / i32x2{6, 1};
+			auto buffer = ByteBuffer(image_file.buffer.size);
+
+			// un-interleve the buffer (basically turns it into above case)
+			auto dst_data = buffer.data_as<u8>();
+			auto dst_idx = 0;
+			auto src_data = image_file.buffer.data_as<u8>();
+			for (auto face = 0; face < 6; ++face)
+			{
+				auto src_idx = face * face_dimensions.x * image_file.channels;
+				for (auto y = 0; y < face_dimensions.y; ++y)
+				{
+					std::memcpy(dst_data + dst_idx, src_data + src_idx, face_dimensions.x * image_file.channels);
+					dst_idx += face_dimensions.x * image_file.channels;
+					src_idx += image_file.dimensions.x * image_file.channels;
+				}
+			}
+
+			loaded_data.face_dimensions = face_dimensions;
+			loaded_data.data = move(buffer);
+		}
+		else if (image_file.dimensions.x == image_file.dimensions.y)
+			assert(false); // equirectangular environment maps are not supported yet
+
+		return loaded_data;
 	}
 
 	GL::TextureCubemap Convert(LoadedData const & loaded)
